66ea107cd318bb0d778588504ec50f2641684e1f
Igor Oluic <igoroluic13@gmail.com>
2024-10-15 02:25:25 +0200

    Improve visibility hook to prevent unnecessary rerenders

--- src/hooks/useVisibilityControl.ts
+++ src/hooks/useVisibilityControl.ts
@@ -3,8 +3,18 @@ import { useState } from 'react';
 const useVisibilityControl = (initiallyOpen?: boolean) => {
   const [isOpen, setIsOpen] = useState(initiallyOpen);
 
-  const onOpen = () => setIsOpen(true);
-  const onClose = () => setIsOpen(false);
+  const onOpen = () => {
+    if (!isOpen) {
+      setIsOpen(true);
+    }
+  };
+
+  const onClose = () => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  };
+
   const onToggle = () => setIsOpen(!isOpen);
 
   return { isOpen, setIsOpen, onOpen, onClose, onToggle };
